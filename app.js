b23cf18fd3edd2da7d6cfe2263931c18b8843268
jolandaquni29 <jolandaquni@gmail.com>
2026-09-23 12:25:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -93,11 +93,11 @@ function populateTimeSlots(appointments) {
       option.value = time;
       option.textContent = time;
     }
-      timeSelect.appendChild(option);
+            timeSelect.appendChild(option);
     }
   }
-
-
+}
+function updateDateTitle() {
 function updateDateTitle() {
   document.getElementById("currentDate").textContent =
     displayDate(selectedDate);
