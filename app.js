cd98ed0df495f1bc71702b4768223143862c25cc
ambulatorigvm <jolandaquni@gmail.com>
2026-09-25 15:28:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("APP START");
+Uncaught TypeError: "" is not a function
+    at app.js?v=GVM-20260923-03:1:3
+(anonymous) @ app.js?v=GVM-20260923-03:1
+favicon.ico:1  GET https://ambulatorigvm.github.io/favicon.ico 404 (Not Found)
